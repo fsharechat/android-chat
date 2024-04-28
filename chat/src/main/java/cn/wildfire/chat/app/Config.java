7cecfbc22058c284b3d61663f39f539dd6d0c164
Renfei <ljlong_2008@126.com>
2024-04-28 18:10:28 +0800

    更新服务地址

--- chat/src/main/java/cn/wildfire/chat/app/Config.java
+++ chat/src/main/java/cn/wildfire/chat/app/Config.java
@@ -9,18 +9,18 @@ import android.os.Environment;
 public interface Config {
 
 //    String IM_SERVER_HOST = "192.168.0.102";
-    String IM_SERVER_HOST = "chat.comsince.cn";
+    String IM_SERVER_HOST = "backend-tcp.fsharechat.cn";
 //    String IM_SERVER_HOST = "172.16.46.201";
     int IM_SERVER_PORT = 6789;
 
 //    String APP_SERVER_HOST = "192.168.0.102";
-    String APP_SERVER_HOST = "chat.comsince.cn";
+    String APP_SERVER_HOST = "backend-http.fsharechat.cn";
 //    String APP_SERVER_HOST = "172.16.46.201";
-    int APP_SERVER_PORT = 8081;
+    int APP_SERVER_PORT = 443;
 
-    String ICE_ADDRESS = "turn:turn.liyufan.win:3478";
-    String ICE_USERNAME = "wfchat";
-    String ICE_PASSWORD = "wfchat";
+    String ICE_ADDRESS = "turn:turn.fsharechat.cn:3478";
+    String ICE_USERNAME = "comsince";
+    String ICE_PASSWORD = "comsince";
 
     int DEFAULT_MAX_AUDIO_RECORD_TIME_SECOND = 120;
 
